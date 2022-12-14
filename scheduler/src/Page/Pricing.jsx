7e9d9465f281bd35ead275999b7eb08805b95fe5
Parag Sonje <107980794+Parag2510@users.noreply.github.com>
2022-12-14 23:09:13 +0530

    day2

---
+++ scheduler/src/Page/Pricing.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Pricing = () => {
+  return (
+    <div>
+          <h1>Supercharge your teamwork. Start <a style={{color:"#6c6cff"}}>free</a>.</h1>
+                <p>Unlimited boards and workflows. No credit card needed.</p>
+          <div>
+             
+          </div>
+
+    </div>
+  )
+}
+
+export default Pricing
