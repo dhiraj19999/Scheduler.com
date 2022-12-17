cd2f3bc89317f1ba810c3998e9b31ae815d93fe1
Parag Sonje <107980794+Parag2510@users.noreply.github.com>
2022-12-17 22:54:26 +0530

    main

--- scheduler/src/Routes/AllRoutes.jsx
+++ scheduler/src/Routes/AllRoutes.jsx
@@ -9,8 +9,8 @@ const AllRoutes = () => {
   return (
     <Routes>
         <Route path='/' element={<HomePage/>}/>
-       {/*  <Route path='/pricing' element={<PricingPage/>}/>
-        <Route path='/contact' element={<ContactPage/>}/> */}
+        <Route path='/pricing' element={<Pricing/>}/>
+        <Route path='/contact' element={<ContactPage/>}/> 
         <Route path='/login' element={<LoginPage/>}/>
         <Route path='/signup' element={<SignupPage/>}/>
     </Routes>
